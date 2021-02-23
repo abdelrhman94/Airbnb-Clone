65710be0b52f2f9378bf591cbc1a8db408753bf4
Abdelrhman Saied <abdelrhman.s.farag@gmail.com>
2021-02-23 19:58:07 +0200

    SearchPage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./components/SearchPage";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
